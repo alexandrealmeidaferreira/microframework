ec85102d08844b5eb07328149ecb6f1569f85070
Alexandre Almeida Ferreira <alexandrealmeidaferreira@gmail.com>
2015-12-09 19:55:08 -0200

    added APP_ENV with default to development

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@
 ini_set('display_errors', 1);
 chdir(dirname(__DIR__));
 
+define('APP_ENV', (getenv('APP_ENV')) ? getenv('APP_ENV') : 'development');
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_DIR', __DIR__ . DS . '..' . DS);
 define('CONFIG_DIR', ROOT_DIR . 'config' . DS);
