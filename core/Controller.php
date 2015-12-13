706eaf00d67a25f1dc9f29f258c54356b4057a45
Alexandre Almeida Ferreira <alexandrealmeidaferreira@gmail.com>
2015-12-13 18:11:52 -0200

    improved Controller params

--- core/Controller.php
+++ core/Controller.php
@@ -26,13 +26,14 @@ class Controller
      * Return all post values if parameter is not passed, or match the variable
      *
      * @param string $var
+     * @param string $default
      * @return array
      */
-    public function getPost($var = '')
+    public function getPost($var = '', $default = '')
     {
         $variables = array();
-        if (!empty($var) && isset($_POST[$var])) {
-            $variables = $_POST[$var];
+        if (!empty($var)) {
+            $variables = isset($_POST[$var]) ? $_POST[$var] : $default;
         } else if (empty($var)) {
             $variables = $_POST;
         }
@@ -43,13 +44,14 @@ class Controller
      * Return all get values if parameter is not passade, or match the variable
      *
      * @param string $var
+     * @param string $default
      * @return array
      */
-    public function getGet($var = '')
+    public function getGet($var = '', $default = '')
     {
         $variables = array();
-        if (!empty($var) && isset($_GET[$var])) {
-            $variables = $_GET[$var];
+        if (!empty($var)) {
+            $variables = isset($_GET[$var]) ? $_GET[$var] : $default;
         } else if (empty($var)) {
             $variables = $_GET;
         }
